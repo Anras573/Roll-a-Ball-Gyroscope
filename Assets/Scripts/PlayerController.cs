545e5804b73c36e3904ba126bfe49611984fc958
Anders Bo Rasmussen <andersborasmussen@hotmail.com>
2017-01-15 00:36:08 +0100

    Roll a Ball with Gyroscope controls.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public Text CountText;
+    public Text WinText;
+
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+    private const string PickUpTag = "Pick Up";
+
+    private Rigidbody _rigidbody;
+    private int _count;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _count = 0;
+        SetText();
+        WinText.text = string.Empty;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis(Horizontal);
+        float moveVertical = Input.GetAxis(Vertical);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rigidbody.AddForce(movement * Speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(PickUpTag))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            SetText();
+        }
+    }
+
+    void SetText()
+    {
+        CountText.text = "Count: " + _count.ToString();
+        if (_count == 12)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private readonly Vector3 _rotation = new Vector3(15f, 30f, 45f);
+
+	void Update () {
+		transform.Rotate(_rotation * Time.deltaTime);
+	}
+}
